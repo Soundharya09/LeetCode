eee14822ffbd9e8e1631dd56582ff2fdad406701
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-05-29 10:26:17 +0530

    Time: 0 ms (100%), Space: 41.4 MB (70.96%) - LeetHub

--- 0891-score-after-flipping-matrix/0891-score-after-flipping-matrix.java
+++ 0891-score-after-flipping-matrix/0891-score-after-flipping-matrix.java
@@ -2,21 +2,21 @@ class Solution {
     public int matrixScore(int[][] arr) {
         int m = arr.length, n = arr[0].length;
         for(int i=0; i<m; i++){
-            if(arr[i][0] == 0){
-                for(int j=0; j<n; j++){
+            if(arr[i][0] == 0){ // flipping row
+                for(int j=0; j<n; j++){ // converting 0 to 1 and vice-versa
                     if(arr[i][j] == 0) arr[i][j] = 1;
                     else arr[i][j] = 0;
                 }
             }
         }
-        for(int j=1; j<n; j++){
+        for(int j=1; j<n; j++){ //flip columns where 0s > 1s
             int noOfZeros = 0, noOfOnes = 0;
             for(int i=0; i<m; i++){
                 if(arr[i][j] == 0) noOfZeros++;
                 else noOfOnes++;
             }
             if(noOfZeros > noOfOnes){
-                for(int i=0; i<m; i++){
+                for(int i=0; i<m; i++){ //converting 0 to 1 and vice-versa
                     if(arr[i][j] == 0) arr[i][j] = 1;
                     else arr[i][j] = 0;
                 }
@@ -25,7 +25,7 @@ class Solution {
         int score = 0;
         int x = 1;
         for(int j=n-1; j>=0; j--){
-            for(int i=0; i<m; i++){
+            for(int i=0; i<m; i++){ //converting binary row to decimal
                 score += (arr[i][j] * x);
             }
             x *= 2;
