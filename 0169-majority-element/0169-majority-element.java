ab593b672f0ab4a4a09b2538ee5b250c26adc826
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-06-15 12:58:03 +0530

    Time: 76 ms (7.34%), Space: 53.2 MB (38.99%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,22 @@
 class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public int majorityElement(int[] nums) {
         //Logic 1
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        // return nums[nums.length / 2];
+
+        //Logic 2
+        for(int i = 1; i < nums.length; i++) {
+            int j = i;
+            while(j >= 1 && nums[j] < nums[j-1]) {
+                swap(nums, j, j-1);
+                j--;
+            }
+        }
         return nums[nums.length / 2];
     }
 }
