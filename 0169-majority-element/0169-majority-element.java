ca920e12beb663261c58f55168505baa1dd95a09
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-06-15 12:54:02 +0530

    Time: 4 ms (49.91%), Space: 53.4 MB (21.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int major_ele = nums[0];
-        int count = 1;
-        for(int i = 1; i < n; i++) {
-            if(count == 0) {
-                major_ele = nums[i];
-                count = 1;
-            }
-            else if(nums[i] == major_ele) count++;
-            else count--;
-        }
-        return major_ele;
+        //Logic 1
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
