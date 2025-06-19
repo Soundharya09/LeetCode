616e0a728594d226ffe2d60f54c55e01bc32a6c4
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-06-19 21:18:55 +0530

    Time: 0 ms (100%), Space: 42.7 MB (6.4%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,25 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+    public int search(int[] arr, int target) {
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == target) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int n = arr.length;
+        int low = 0, high = n-1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target) return mid;
+            else if(arr[mid] < arr[high]) { // At right sorted array
+                if(target > arr[mid] && target <= arr[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+            else { // At left sorted array
+                if(target >= arr[low] && target <= arr[mid]) high = mid - 1;
+                else low = mid + 1;
             }
         }
         return -1;
-        
     }
 }
