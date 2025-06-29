72ddbe9105c8397cbb3653f24b7b3b2f2fcf50ef
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-06-29 16:02:12 +0530

    Time: 7 ms (45.47%), Space: 40 MB (98.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,13 +1,13 @@
 class Solution {
-    public static void main(String[] args){
-        int n = 8;
-        int ans = fib(8);
-        System.out.print(ans);
-    }
     public static int fib(int n){
         if(n <= 1) return n;
         return fib(n-1) + fib(n-2);
     }
+    public static void main(String[] args){
+        int n = 8;
+        int ans = fib(8);
+        System.out.print(ans);
+    }   
 }
 //     public int fib(int n) {
 //         int[] memo = new int[31];
