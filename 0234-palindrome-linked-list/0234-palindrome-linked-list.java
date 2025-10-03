305b14226bb31829b1ccd987c82efcf947012bdc
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-10-03 19:26:06 +0530

    Time: 4 ms (83.08%), Space: 68.7 MB (33.3%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,23 +1,38 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         //Method - 1 -> deep copy of LL
-        if(head.next == null) return true;
-        ListNode newHead = new ListNode(head.val);
-        ListNode t1 = head.next;
-        ListNode t2 = newHead;
-        while(t1 != null) {
-            ListNode temp = new ListNode(t1.val);
-            t2.next = temp;
-            t2 = t2.next;
-            t1 = t1.next;
+    //     if(head.next == null) return true;
+    //     ListNode newHead = new ListNode(head.val);
+    //     ListNode t1 = head.next;
+    //     ListNode t2 = newHead;
+    //     while(t1 != null) {
+    //         ListNode temp = new ListNode(t1.val);
+    //         t2.next = temp;
+    //         t2 = t2.next;
+    //         t1 = t1.next;
+    //     }
+    //     newHead = reverseList(newHead);
+    //     t1 = head;
+    //     t2 = newHead;
+    //     while(t1 != null) {
+    //         if(t1.val != t2.val) return false;
+    //         t2 = t2.next;
+    //         t1 = t1.next;
+    //     }
+    //     return true;
+        //Method - 2 -> Middle & Reverse of LL with two-pointer approach
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        newHead = reverseList(newHead);
-        t1 = head;
-        t2 = newHead;
-        while(t1 != null) {
-            if(t1.val != t2.val) return false;
-            t2 = t2.next;
-            t1 = t1.next;
+        ListNode j = reverseList(slow);
+        ListNode i = head;
+        while(j != null) {
+            if(i.val != j.val) return false;
+            i = i.next;
+            j = j.next;
         }
         return true;
     }
