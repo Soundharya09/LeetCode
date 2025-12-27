c3b6fa684715a005450ee4c996cd1aeee5488c12
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-12-27 11:02:58 +0530

    Time: 3 ms (4.31%), Space: 45.4 MB (9.14%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -2,7 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         for(int num : nums) {
-            int x = String.valueOf(num).length(); //convert int to string -> 345= "345"
+            int x = String.valueOf(num).length(); //convert int to string -> 345 = "345" = 3. This string count will be stored in x.
             if(x % 2 == 0) count++;
         }
         return count;
