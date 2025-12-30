ae0948ae489d30323cef08171067e74a8d883e9f
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2025-12-30 19:52:19 +0530

    Time: 2 ms (10.99%), Space: 42.9 MB (82.61%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,10 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        int lastIndex = s.lastIndexOf(' ');
-        return s.length() - lastIndex - 1;
+        // s = s.trim();
+        // int lastIndex = s.lastIndexOf(' ');
+        // return s.length() - lastIndex - 1;
+
+        String[] ans = s.split("\\s+");
+        return ans[ans.length - 1].length();
     }
 }
