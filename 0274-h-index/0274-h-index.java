2382b85bb06a8bf591477764176756e5fa1c5790
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-01-02 10:41:26 +0530

    Time: 6 ms (21.98%), Space: 43.6 MB (24.43%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+        int left = 0, right = n;
+        while(left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if(citations[n - mid] >= mid) left = mid;
+            else right = mid - 1;
+        }
+        return left;
+    }
+}
