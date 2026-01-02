3501eabcaddf85c0ae2197b507251d738f6c86b9
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-01-02 10:54:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (69.81%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,13 +1,29 @@
 class Solution {
     public int hIndex(int[] citations) {
+   //Approach - 1 -> Using for loop and sorting techniques, T.C. -> O(nlogn), S.C. -> O(1)
+        //Approach - 2 -> Binary Search -> T.C. -> O(nlogn), S.C. -> O(1)
+        // int n = citations.length;
+        // Arrays.sort(citations);
+        // int left = 0, right = n;
+        // while(left < right) {
+        //     int mid = left + (right - left + 1) / 2;
+        //     if(citations[n - mid] >= mid) left = mid;
+        //     else right = mid - 1;
+        // }
+        // return left;
+
+        // Approach - 3 -> Using Count Sort -> T.C. -> O(n), S.C. -> O(n), But Optimal
         int n = citations.length;
-        Arrays.sort(citations);
-        int left = 0, right = n;
-        while(left < right) {
-            int mid = left + (right - left + 1) / 2;
-            if(citations[n - mid] >= mid) left = mid;
-            else right = mid - 1;
+        int[] bucket = new int[n + 1];
+        for(int c : citations) {
+            if(c >= n) bucket[n]++;
+            else bucket[c]++;
         }
-        return left;
+        int total = 0;
+        for(int i = n; i >= 0; i--) {
+            total += bucket[i];
+            if(total >= i) return i;
+        }
+        return 0;
     }
 }
