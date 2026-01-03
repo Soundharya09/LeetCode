817ee8b5ae65e674702b9bfb81f469f6ff66aae8
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-01-03 12:50:48 +0530

    Time: 1 ms (91.23%), Space: 45.4 MB (40.14%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,12 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[i] == nums[j]) count++;
-            }
-            if(count == 1) return nums[i];
+        // Approach - 1 -> Brute-Force
+        // for(int i = 0; i < nums.length; i++) {
+        //     int count = 0;
+        //     for(int j = 0; j < nums.length; j++) {
+        //         if(nums[i] == nums[j]) count++;
+        //     }
+        //     if(count == 1) return nums[i];
+        // }
+        // return -1;
+
+        // Approach - 2 -> Bit Manipulation
+        int ones = 0, twos = 0;
+        for(int x : nums) {
+            ones = (ones ^ x) & ~twos;
+            twos = (twos ^ x) & ~ones;
         }
-        return -1;
+        return ones;
     }
 }
