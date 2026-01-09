45cc0c8f8b9a84966efb7119179de2ba59325644
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-01-09 10:54:46 +0530

    Time: 1 ms (45.12%), Space: 46.4 MB (46.19%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,29 @@
+class Solution {
+    // Helper class
+    private static class Pair {
+        int depth;
+        TreeNode lca;
+        Pair(int depth, TreeNode lca) {
+            this.depth = depth;
+            this.lca = lca;
+        }
+    }
+    public Pair findLcaAndDepth(TreeNode node) {
+        if(node == null) return new Pair(0, null);
+        // Leaf Node
+        if(node.left == null && node.right == null) return new Pair(1, node);
+        Pair left = findLcaAndDepth(node.left);
+        Pair right = findLcaAndDepth(node.right);
+        int maxDepth = Math.max(left.depth, right.depth);
+        TreeNode currLca;
+
+        if(left.depth == right.depth) currLca = node;
+        else if(left.depth > right.depth) currLca = left.lca;
+        else currLca = right.lca;
+
+        return new Pair(maxDepth + 1, currLca);
+    }
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return findLcaAndDepth(root).lca;
+    }
+}
