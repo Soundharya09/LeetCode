e340376e4930ed5b7cd303b72a31597154037480
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-01-10 10:51:41 +0530

    Time: 2 ms (59.83%), Space: 44.4 MB (28.13%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        long areaA = (long)(ax2 - ax1) * (ay2 - ay1);
+        long areaB = (long)(bx2 - bx1) * (by2 - by1);
+
+        int left = Math.max(ax1, bx1);
+        int right = Math.min(ax2, bx2);
+        int bottom = Math.max(ay1, by1);
+        int top = Math.min(ay2, by2);
+
+        long overlap = 0;
+        if(left < right && bottom < top) overlap = (long)(right - left) * (top - bottom);
+        return (int)(areaA + areaB - overlap);
+    }
+}
