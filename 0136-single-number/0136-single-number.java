1943c15c33288b14c833123535ceca8ef47ea1f7
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-01-11 12:21:19 +0530

    Time: 1 ms (99.85%), Space: 47.2 MB (10.47%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for(int i = 0; i < n-1; i += 2) {
-            if(nums[i] != nums[i+1]) return nums[i];
+        // Approach - 1 -> Brute-Force Approach
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // for(int i = 0; i < n-1; i += 2) {
+        //     if(nums[i] != nums[i+1]) return nums[i];
+        // }
+        // return nums[n - 1];
+
+        // Approach - 2 -> XOR Approach
+        int res = 0;
+        for(int num : nums) {
+            res ^= num;
         }
-        return nums[n - 1];
+        return res;
     }
 }
