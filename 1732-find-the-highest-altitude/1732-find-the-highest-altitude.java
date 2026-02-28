57761d964603868566068fe61e1388a18df317e3
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-02-28 09:15:12 +0530

    Time: 0 ms (100%), Space: 43.1 MB (74.92%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,22 +1,22 @@
 class Solution {
     public int largestAltitude(int[] gain) {
         // Approach - 1 Brute Force 
-        int max = 0, current = 0;
-        for(int g : gain) {
-            current += g;
-            max = Math.max(max, current);
-        }
-        return max;
-
-        // Approach - 2 Prefix Sum
-        // int[] prefix = new int[gain.length + 1];
-        // for(int i = 1; i <= gain.length; i++) {
-        //     prefix[i] = prefix[i - 1] + gain[i - 1];
-        // }
-        // int max = 0;
-        // for(int val : prefix) {
-        //     max = Math.max(max, val);
+        // int max = 0, current = 0;
+        // for(int g : gain) {
+        //     current += g;
+        //     max = Math.max(max, current);
         // }
         // return max;
+
+        // Approach - 2 Prefix Sum
+        int[] prefix = new int[gain.length + 1];
+        for(int i = 1; i <= gain.length; i++) {
+            prefix[i] = prefix[i - 1] + gain[i - 1];
+        }
+        int max = 0;
+        for(int val : prefix) {
+            max = Math.max(max, val);
+        }
+        return max;
     }
 }
