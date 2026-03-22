a874657f737d9ba13be1851d3dd9dcec006ff46d
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-03-22 10:36:41 +0530

    Time: 1 ms (84.36%), Space: 42.6 MB (90.4%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+        int k = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) k = k * 10 + (c - '0');
+            else if (c == '[') {
+                countStack.push(k);
+                stringStack.push(current);
+                current = new StringBuilder();
+                k = 0;
+            } 
+            else if (c == ']') {
+                int repeat = countStack.pop();
+                StringBuilder decoded = stringStack.pop();
+                for (int i = 0; i < repeat; i++) {
+                    decoded.append(current);
+                }
+                current = decoded;
+            } 
+            else current.append(c);
+        }
+        return current.toString();
+    }
+}
