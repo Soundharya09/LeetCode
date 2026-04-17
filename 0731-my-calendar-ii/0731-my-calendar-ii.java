d088e3274e66889c23365ea9728305eb81c3743a
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-04-17 11:47:26 +0530

    Time: 47 ms (77.4%), Space: 47.3 MB (73.19%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -0,0 +1,30 @@
+class MyCalendarTwo {
+    private List<int[]> bookings;
+    private List<int[]> overlaps;
+
+    public MyCalendarTwo() {
+        bookings = new ArrayList<>();
+        overlaps = new ArrayList<>();
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        for (int[] overlap : overlaps) {
+            if (startTime < overlap[1] && endTime > overlap[0]) return false;
+        }
+        for (int[] booking : bookings) {
+            int overlapStart = Math.max(startTime, booking[0]);
+            int overlapEnd = Math.min(endTime, booking[1]);
+            if (overlapStart < overlapEnd) {
+                overlaps.add(new int[]{overlapStart, overlapEnd});
+            }
+        }
+        bookings.add(new int[]{startTime, endTime});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
