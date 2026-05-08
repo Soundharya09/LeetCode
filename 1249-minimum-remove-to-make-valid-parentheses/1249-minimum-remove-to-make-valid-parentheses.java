666d132e4652f6097590fdfb1231defe4c935854
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-05-08 09:33:53 +0530

    Time: 19 ms (56.75%), Space: 47.4 MB (49.42%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> toRemove = new HashSet<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') stack.push(i);
+            else if (c == ')') {
+                if (stack.isEmpty()) toRemove.add(i);
+                else stack.pop();
+            }
+        }
+        while (!stack.isEmpty()) {
+            toRemove.add(stack.pop());
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!toRemove.contains(i)) sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
