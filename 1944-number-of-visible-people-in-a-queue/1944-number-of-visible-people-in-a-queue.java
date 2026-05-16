6c41525c1adae525dbe36d42273b961fcadf55e3
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-05-16 13:17:04 +0530

    Time: 71 ms (5.25%), Space: 116.8 MB (12.86%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -1,19 +1,31 @@
 class Solution {
     public int[] canSeePersonsCount(int[] heights) {
-        int n = heights.length;
-        int[] answer = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = n - 1; i >= 0; i--) {
-            int count = 0;
-            while (!stack.isEmpty() && stack.peek() < heights[i]) {
-                stack.pop();
-                count++;
-            }
-            if (!stack.isEmpty()) count++;
+        // int n = heights.length;
+        // int[] answer = new int[n];
+        // Deque<Integer> stack = new ArrayDeque<>();
+        // for (int i = n - 1; i >= 0; i--) {
+        //     int count = 0;
+        //     while (!stack.isEmpty() && stack.peek() < heights[i]) {
+        //         stack.pop();
+        //         count++;
+        //     }
+        //     if (!stack.isEmpty()) count++;
             
-            answer[i] = count;
-            stack.push(heights[i]);
+        //     answer[i] = count;
+        //     stack.push(heights[i]);
+        // }
+        // return answer;
+        int n = heights.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++) {
+            while(!st.isEmpty() && heights[st.peek()] <= heights[i]) {
+                res[st.peek()]++;
+                st.pop();
+            }
+            if(!st.isEmpty()) res[st.peek()]++;
+            st.push(i);
         }
-        return answer;
+        return res;
     }
 }
