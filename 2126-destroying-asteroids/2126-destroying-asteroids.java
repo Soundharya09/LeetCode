3c03df904afa438c53cc8d5b4090393e626d0718
SOUNDHARYA SUNDARAM IYER <122423435+Soundharya09@users.noreply.github.com>
2026-05-31 08:52:31 +0530

    Time: 26 ms (81.52%), Space: 107.7 MB (7.85%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long planetMass = mass;
+        for(int asteroid : asteroids) {
+            if(planetMass < asteroid) return false;
+            planetMass += asteroid;
+        }
+        return true;
+    }
+}
